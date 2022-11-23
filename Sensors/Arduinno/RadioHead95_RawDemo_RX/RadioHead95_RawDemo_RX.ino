bd83e4f65e2bf7f4cee819bec2d70b3193fec9ba
MisterB33 <61337503+MisterB33@users.noreply.github.com>
2022-11-23 12:18:15 -0800

    Add files via upload

---
+++ Sensors/Arduinno/RadioHead95_RawDemo_RX/RadioHead95_RawDemo_RX.ino
@@ -0,0 +1,119 @@
+// rf95 demo tx rx.pde
+// -*- mode: C++ -*-
+// Majid Reza Barghi 
+// Sensors Labs for Ohlone College 
+// This is Simple example sketch showing how to create a simple messageing client
+// with the RH_RF95 class. RH_RF69 class does not provide for addressing or
+// reliability, so you should only use RH_RF95  if you do not need the higher
+// level messaging abilities.
+// UPDATED TO WORK WITH RH95 BOARD. RESET PIN NEEDS TO BE PULLED HIGH FOR IT TO WORK PROPERLY. 
+// It is designed to work with the other example rf95_server.
+// The ENcryption has beeen removed because it is unvaible in the current RF95 Library, can easily be added.
+#define __AVR_ATmega328P__
+#include <SPI.h>
+#include <RH_RF95.h>
+
+/************ Radio Setup ***************/
+
+// Change to 915.0 or other frequency, must match RX's freq!
+#define RF69_FREQ 915.0
+
+#if defined (__AVR_ATmega328P__)  // Feather 328P w/wing
+  #define RFM69_INT     3  // 
+  #define RFM69_CS      4  //
+  #define RFM69_RST     2  // "A"
+  #define LED           13
+#endif
+
+// Singleton instance of the radio driver
+// Kept the same name from sample code to make this easier. 
+RH_RF95 rf69(RFM69_CS, RFM69_INT);
+
+int16_t packetnum = 0;  // packet counter, we increment per xmission
+
+void setup() 
+{
+  Serial.begin(115200);
+  //while (!Serial) { delay(1); } // wait until serial console is open, remove if not tethered to computer
+
+  pinMode(LED, OUTPUT);     
+  pinMode(RFM69_RST, OUTPUT);
+  digitalWrite(RFM69_RST, LOW);
+
+  Serial.println("Feather RFM69 RX Test!");
+  Serial.println();
+
+  // manual reset
+  digitalWrite(RFM69_RST, HIGH);
+  delay(10);
+  digitalWrite(RFM69_RST, LOW);
+  delay(10);
+  digitalWrite(RFM69_RST, HIGH);
+  
+  if (!rf69.init()) {
+    Serial.println("RFM69 radio init failed");
+    while (1);
+  }
+  Serial.println("RFM69 radio init OK!");
+  
+  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM (for low power module)
+  // No encryption
+  if (!rf69.setFrequency(RF69_FREQ)) {
+    Serial.println("setFrequency failed");
+  }
+
+  // If you are using a high power RF69 eg RFM69HW, you *must* set a Tx power with the
+  // ishighpowermodule flag set like this:
+  rf69.setTxPower(20, true);  // range from 14-20 for power, 2nd arg must be true for 69HCW
+
+  // The encryption key has to be the same as the one in the server
+  uint8_t key[] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
+                    0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08};
+  //Encryption removed, the option can easily be placed with the by just copying and pasting it from teh rf69 library to rf95 lib. 
+  // You just need the method, it has nothing to do with the board itself. 
+  //rf69.setEncryptionKey(key);
+  
+  pinMode(LED, OUTPUT);
+
+  Serial.print("RFM69 radio @");  Serial.print((int)RF69_FREQ);  Serial.println(" MHz");
+}
+
+//Maybe adjust the maximum length for the code. 
+void loop() {
+ if (rf69.available()) {
+    // Should be a message for us now   
+    uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+    uint8_t len = sizeof(buf);
+    if (rf69.recv(buf, &len)) {
+      if (!len) return;
+      buf[len] = 0;
+      Serial.print("Received [");
+      Serial.print(len);
+      Serial.print("]: ");
+      Serial.println((char*)buf);
+      Serial.print("RSSI: ");
+      Serial.println(rf69.lastRssi(), DEC);
+
+      if (strstr((char *)buf, "This is Reza's hard work paying off")) {
+        // Send a reply!
+        uint8_t data[] = "And hello back to you";
+        rf69.send(data, sizeof(data));
+        rf69.waitPacketSent();
+        Serial.println("Sent a reply");
+        Blink(LED, 40, 3); //blink LED 3 times, 40ms between blinks
+      }
+    } else {
+      Serial.println("Receive failed");
+    }
+  }
+}
+
+
+void Blink(byte PIN, byte DELAY_MS, byte loops) {
+  for (byte i=0; i<loops; i++)  {
+    digitalWrite(PIN,HIGH);
+    delay(DELAY_MS);
+    digitalWrite(PIN,LOW);
+    delay(DELAY_MS);
+  }
+}
